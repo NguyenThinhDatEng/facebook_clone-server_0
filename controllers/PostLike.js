6da1d7ce2a16599b0a0d21484302ff21d1371176
NguyenThinhDatEng <thinh.nv194178@sis.hust.edu.vn>
2023-01-31 16:45:00 +0700

    update like api

--- controllers/PostLike.js
+++ controllers/PostLike.js
@@ -4,39 +4,67 @@ const PostModel = require("../models/Posts");
 const postLikeController = {};
 
 postLikeController.action = async (req, res, next) => {
-    try {
-        let userId = req.userId;
-        let post = await PostModel.findById(req.params.postId);
-        if (post == null) {
-            return res.status(httpStatus.NOT_FOUND).json({message: "Can not find post"});
-        }
+  try {
+    // Lấy id người dùng từ token
+    let userId = req.userId;
 
-        let arrLike = post.like;
-        let arrLikeNotContainCurrentUser = arrLike.filter((item) => {
-            return item != userId
-        });
-        if (arrLikeNotContainCurrentUser.length === arrLike.length) {
-            arrLike.push(userId);
-        } else {
-            arrLike = arrLikeNotContainCurrentUser;
-        }
-        post = await PostModel.findOneAndUpdate({_id: req.params.postId}, {
-            like: arrLike
-        }, {
-            new: true,
-            runValidators: true
-        }).populate('like', ['username', 'phonenumber']);
-
-        if (!post) {
-            return res.status(httpStatus.NOT_FOUND).json({message: "Can not find post"});
-        }
-        post.isLike = post.like.includes(req.userId);
-        return res.status(httpStatus.OK).json({
-            data: post
-        });
-    } catch (error) {
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message: error.message});
+    // Kiểm tra sự tồn tại của id bài viết
+    let post = await PostModel.findById(req.params.postId);
+    if (post == null) {
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: "Can not find post" });
     }
-}
+
+    // Gán mảng các thông tin người dùng đã like bài viết
+    let arrLike = post.like;
+    // Kiểm tra xem người dùng hiện tại đã like bài viết chưa
+    let arrLikeNotContainCurrentUser = arrLike.filter((item) => {
+      return item._id != userId;
+    });
+    // Nếu chưa like => Thêm id vào
+    if (arrLikeNotContainCurrentUser.length === arrLike.length) {
+      arrLike.push(userId);
+    } else {
+      // Nếu like rồi => Bỏ id đi
+      arrLike = arrLikeNotContainCurrentUser;
+    }
+
+    // Cập nhật bài viết
+    post = await PostModel.findOneAndUpdate(
+      { _id: req.params.postId },
+      {
+        like: arrLike,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    ).populate("like", ["username", "phonenumber"]);
+
+    if (!post) {
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: "Can not find post" });
+    }
+
+    // Cập nhật trạng thái ban đầu của nút like
+    post.like.forEach((element) => {
+      if (element._id == req.userId) {
+        post.isLike = true;
+      } else {
+        post.isLike = false;
+      }
+    });
+
+    return res.status(httpStatus.OK).json({
+      data: post,
+    });
+  } catch (error) {
+    return res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
+};
 
 module.exports = postLikeController;
--- controllers/Posts.js
+++ controllers/Posts.js
@@ -12,68 +12,18 @@ const uploadFile = require("../functions/uploadFile");
 
 const postsController = {};
 postsController.create = async (req, res, next) => {
+  let userId = req.userId;
   try {
-    const { userId, described, images, videos } = req.body;
-    // let dataImages = [];
-    // if (Array.isArray(images)) {
-    //     for (const image of images) {
-    //         if (uploadFile.matchesFileBase64(image) !== false) {
-    //             const imageResult = uploadFile.uploadFile(image);
-    //             if (imageResult !== false) {
-    //                 let imageDocument = new DocumentModel({
-    //                     fileName: imageResult.fileName,
-    //                     fileSize: imageResult.fileSize,
-    //                     type: imageResult.type
-    //                 });
-    //                 let savedImageDocument = await imageDocument.save();
-    //                 if (savedImageDocument !== null) {
-    //                     dataImages.push(savedImageDocument._id);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-
-    // let dataVideos = [];
-    // if (Array.isArray(videos)) {
-    //     for (const video of videos) {
-    //         if (uploadFile.matchesFileBase64(video) !== false) {
-    //             const videoResult = uploadFile.uploadFile(video);
-    //             if (videoResult !== false) {
-    //                 let videoDocument = new DocumentModel({
-    //                     fileName: videoResult.fileName,
-    //                     fileSize: videoResult.fileSize,
-    //                     type: videoResult.type
-    //                 });
-    //                 let savedVideoDocument = await videoDocument.save();
-    //                 if (savedVideoDocument !== null) {
-    //                     dataVideos.push(savedVideoDocument._id);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+    const { described, images, videos } = req.body;
 
     const post = new PostModel({
       author: userId,
       described: described,
-      // images: dataImages,
-      // videos: dataVideos,
       images: images,
       videos: videos,
       countComments: 0,
     });
-    // let postSaved = (await post.save()).populate('images').populate('videos');
-    // postSaved = await PostModel.findById(postSaved._id).populate('images', ['fileName']).populate('videos', ['fileName']).populate({
-    //     path: 'author',
-    //     select: '_id username phonenumber avatar',
-    //     model: 'Users',
-    //     populate: {
-    //         path: 'avatar',
-    //         select: '_id fileName',
-    //         model: 'Documents',
-    //     },
-    // });
+
     const savedPost = await post.save();
     return res.status(httpStatus.OK).json({
       data: savedPost,
@@ -245,6 +195,9 @@ postsController.list = async (req, res, next) => {
       //     },
       // });
       posts = await PostModel.find({ author: req.query.userId })
+        .sort({
+          updatedAt: -1,
+        })
         .skip(parseInt(req.query.skip))
         .limit(parseInt(req.query.limit));
     } else {
@@ -286,6 +239,9 @@ postsController.list = async (req, res, next) => {
       posts = await PostModel.find({
         author: listIdFriends,
       })
+        .sort({
+          updatedAt: -1,
+        })
         .skip(parseInt(req.query.skip))
         .limit(parseInt(req.query.limit));
     }
